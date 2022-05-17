fdf7f169760afbf3309ed305b1dae1f7c29b043f
sag7214 <sagyahzac@gmail.com>
2022-05-17 13:26:30 +0200

    ilk degisiklik

--- src/kisiselKullanim/GitHubKullanimi1.java
+++ src/kisiselKullanim/GitHubKullanimi1.java
@@ -1,10 +1,13 @@
 package kisiselKullanim;
 
-public class gitHubKullanimi1 {
+public class GitHubKullanimi1 {
 
     public static void main(String[] args) {
 
         System.out.println("hello git hub");
+        System.out.println("That s it");
+
+
 
     }
 
